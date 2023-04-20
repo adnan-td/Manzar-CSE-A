4bfd022ce9006544b77a14c482aecff38ae00b1c
Adnan Shabbir Husain <generaladnan139@gmail.com>
2023-04-20 19:13:33 +0530

    Site Integrated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
-}
+};
